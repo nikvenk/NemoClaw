a51fa62e6a770fba89d10d289611fa626b3f84bb
Miyoung Choi <miyoungc@nvidia.com>
2026-04-03 14:35:52 -0700

    docs: add redirect

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,12 @@ extensions = [
     "sphinxcontrib.mermaid",
     "json_output",
     "search_assets",
+    "sphinx_reredirects",
 ]
+
+redirects = {
+    "reference/inference-profiles": "../inference/inference-options.html",
+}
 
 autodoc_default_options = {
     "members": True,
@@ -94,7 +99,7 @@ html_extra_path = ["project.json"]
 highlight_language = "console"
 
 html_theme_options = {
-    # "public_docs_features": True, # TODO: Uncomment this when the docs are public
+    "public_docs_features": True,
     "announcement": (
         "&#x1F514; NVIDIA NemoClaw is <strong>alpha software</strong>. APIs and behavior"
         " may change without notice. Do not use in production."
