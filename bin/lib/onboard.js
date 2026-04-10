6f18f8c14c8ce54dae8236190489933396eae7d2
chandler-barlow <cbarlow@nvidia.com>
2026-04-09 17:15:15 -0700

    wip

--- bin/lib/onboard.js
+++ bin/lib/onboard.js
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 const mod = require("../../dist/lib/onboard");
+
 module.exports = {
   buildProviderArgs: mod.buildProviderArgs,
   buildSandboxConfigSyncScript: mod.buildSandboxConfigSyncScript,
