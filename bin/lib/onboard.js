676a69ab492475db6afd8393d5d13efce68ae387
Kevin Jones <kejones@nvidia.com>
2026-04-09 20:30:57 -0400

    chore(onboard): restore legacy shim parity with main

--- bin/lib/onboard.js
+++ bin/lib/onboard.js
@@ -1,6 +1,7 @@
 // @ts-nocheck
 // SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
+
 const mod = require("../../dist/lib/onboard");
 module.exports = {
   buildProviderArgs: mod.buildProviderArgs,
